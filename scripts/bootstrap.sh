8430b1aa3ca35def41655eda2f495dcd24cdad74
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-09-13 16:22:20 +0300

    imports

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,7 @@
+flux bootstrap github \
+  --token-auth \
+  --owner=juozasg \
+  --repository=flux-client-perf \
+  --branch=main \
+  --path=infra \
+  --personal
--- scripts/generate-helm-error.sh
+++ scripts/generate-helm-error.sh
@@ -4,3 +4,11 @@
 # when you reconcile the helmrelease, it should reconcile the chart immediately and you should be able to tell the chart isn't ready
 # you might need to reconcile the helmrelease twice
 # reconciling the helmrepository will likely not solve it (might not do anything at all)
+# demonstrate push alerts
+
+
+# demonstrate some troubleshooting steps with vscode
+# ex: https://fluxcd.io/flux/cheatsheets/troubleshooting/#changes-not-being-applied
+# https://fluxcd.io/flux/cheatsheets/troubleshooting/#how-to-debug-not-ready-errors
+# use controller logs
+# "suspend gitops, do some manual changes. see that it works live. resume gitops"
