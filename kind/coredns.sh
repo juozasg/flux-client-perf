5f9fee50fffbacb30385d45a5ad4cd85e9a9ca36
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-11-01 17:46:17 +0200

    kind setup

---
+++ kind/coredns.sh
@@ -0,0 +1,26 @@
+kind create cluster --config=ingress-cluster.yaml
+lsof -i tcp:443
+
+# coredns
+
+# https://github.com/kingdon-ci/fleet-infra/blob/main/examples/production-basic/coredns/coredns-custom.yaml
+
+
+k edit cm coredns -oyaml -n kube-system
+
+rewrite name weavegitops.demo ingress-nginx-controller.ingress-nginx.svc.cluster.local
+rewrite name podinfo.demo ingress-nginx-controller.ingress-nginx.svc.cluster.local
+
+k rollout restart deployment coredns -n kube-system
+
+
+kubectl delete pod dnstools; kubectl run -it --rm --restart=Never --image=infoblox/dnstools:latest dnstools
+
+k attach dnstools -it
+
+
+
+
+
+
+#https://github.com/kingdon-ci/fleet-infra/blob/main/examples/production-basic/coredns/coredns-custom.yaml
